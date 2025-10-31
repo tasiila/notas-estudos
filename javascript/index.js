b92a69269ee743947733fa837f4bac258730b090
Tasila <tasilamourao2002@gmail.com>
2025-10-30 22:13:29 -0300

    finalizar a aula

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,13 @@
-console.log "(Olá mundo)"
+console.log ("Olá mundo")
+
+//criando variavel
+//declarando variavel
+let numero = 102
+console.log (numero)
+numero = 99
+console.log (numero)
+
+const nome = "Seu nome"
+console.log (nome)
+//nome = "batata" - nao pode atribuir valor 
+// parar constante depois de declarar 
