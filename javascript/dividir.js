889bdc65e54b1eb924965b43c4472edf521816cf
tasila <tasilamourao2002@gmail.com>
2025-11-06 21:53:21 -0300

    fazendo exercicios 1 a 2

---
+++ javascript/dividir.js
@@ -0,0 +1,10 @@
+// FUP para dividir dois numeros 
+const num = 10
+const num2 = 4
+
+const resultado = num/num2
+
+console.log('Dividindo dois numeros') 
+console.log('Dividindo '+num+' por '+num2)
+console.log(resultado)
+//comment dividir
